86051cdc145fb74496bfdfae5f542f983da8e745
Clara Nobre <aclaranobre@gmail.com>
2017-09-27 15:11:37 -0300

    corrigindo leitura da imagem

--- static/javascript/bundle.js
+++ static/javascript/bundle.js
@@ -39886,7 +39886,7 @@ const participants = {
 	    key: 'getBase',
 	    value: function getBase(callback) {
 	      var image = document.createElement('img');
-	      image.src = 'static/images/base.png';
+	      image.src = 'static/images/base.jpg';
 	      image.onload = callback.bind(this, image);
 	    }
 	  }, {
