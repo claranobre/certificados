4ade1d54d86c16bc6d65979856fac4d931895a80
Clara Nobre <aclaranobre@gmail.com>
2017-11-03 16:13:01 -0300

    Ajuste da localização dos nomes

--- dev/js/components/certificate.jsx
+++ dev/js/components/certificate.jsx
@@ -34,11 +34,11 @@ class Certificate extends React.Component {
     }
     drawName(context) {
       //let hours = '6 horas';
-      context.font = '60px Arial';
+      context.font = '50px Arial';
       context.fillStyle = '#444242';
       let textSize = context.measureText(this.name);
       let xPos = (this.width - textSize.width) / 2;
-      context.fillText(this.name, xPos, 900);
+      context.fillText(this.name, xPos, 200);
       // textSize = context.measureText(hours);
       // xPos = (this.width - textSize.width - 100) / 2;
       // context.fillText(hours, xPos, 1180);
